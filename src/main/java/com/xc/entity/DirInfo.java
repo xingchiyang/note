8e480518822d2f723c087849037d2d54ee620ebc
yangbian <yangbian@broada.com>
2017-02-22 16:49:58 +0800

    实体类提交

--- src/main/java/com/xc/entity/DirInfo.java
+++ src/main/java/com/xc/entity/DirInfo.java
@@ -1,11 +1,22 @@
 package com.xc.entity;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Date;
 
 /**
  * Created by Administrator on 2017/02/22 0022.
  */
+@Entity
+@Table(name = "dir")
 public class DirInfo {
+	@Id
+	@GenericGenerator(name = "dirInfoId", strategy = "uuid")
+	@GeneratedValue(generator = "dirInfoId")
 	private String id;
 	private String name;
 	private String parentId; // 父类id
@@ -14,4 +25,68 @@ public class DirInfo {
 	private Date createTime;
 	private String modifyUserId;
 	private Date modifyTime;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getParentId() {
+		return parentId;
+	}
+
+	public void setParentId(String parentId) {
+		this.parentId = parentId;
+	}
+
+	public Integer getType() {
+		return type;
+	}
+
+	public void setType(Integer type) {
+		this.type = type;
+	}
+
+	public String getCreateUserId() {
+		return createUserId;
+	}
+
+	public void setCreateUserId(String createUserId) {
+		this.createUserId = createUserId;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public String getModifyUserId() {
+		return modifyUserId;
+	}
+
+	public void setModifyUserId(String modifyUserId) {
+		this.modifyUserId = modifyUserId;
+	}
+
+	public Date getModifyTime() {
+		return modifyTime;
+	}
+
+	public void setModifyTime(Date modifyTime) {
+		this.modifyTime = modifyTime;
+	}
 }
--- src/main/java/com/xc/entity/Note.java
+++ src/main/java/com/xc/entity/Note.java
@@ -1,13 +1,71 @@
 package com.xc.entity;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
  * Created by Administrator on 2017/02/22 0022.
  */
+@Entity
 public class Note {
+	@Id
+	@GenericGenerator(name = "noteId", strategy = "uuid")
+	@GeneratedValue(generator = "noteId")
 	private String id;
 	private String dirId;
 	private String title; // 标题
 	private String content; // 内容
 	private Integer type; // 笔记类型，markdown、常规笔记
 	private String path; // 笔记所在系统文件中的目录
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getDirId() {
+		return dirId;
+	}
+
+	public void setDirId(String dirId) {
+		this.dirId = dirId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Integer getType() {
+		return type;
+	}
+
+	public void setType(Integer type) {
+		this.type = type;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
 }
