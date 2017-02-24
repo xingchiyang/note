5a876e013a7374df2f260ad368ae8627b700c666
yangbian <yangbian@broada.com>
2017-02-24 20:06:15 +0800

    用mybatis替换掉hibernate尝试

--- src/main/java/com/xc/dao/NoteDao.java
+++ src/main/java/com/xc/dao/NoteDao.java
@@ -1,17 +1,16 @@
 package com.xc.dao;
 
-import com.xc.entity.Note;
-import org.apache.ibatis.annotations.*;
+		import com.xc.entity.Note;
+		import org.apache.ibatis.annotations.*;
 
 /**
  * Created by Administrator on 2017/02/22 0022.
  */
-@Mapper
 public interface NoteDao {
 
 	public void insert(Note note);
 
-//	public void update(Note note);
+	//	public void update(Note note);
 
 	public Note selectNoteById(@Param("id") String id);
 }
