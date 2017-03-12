59db619f2344f9d5051f545d18b94f270b9dac9f
xingchiyang <827019218@qq.com>
2017-03-12 16:25:16 +0800

    前端树形控件用jstree，后端调整

--- src/main/java/com/xc/api/service/impl/FileFrontServiceImpl.java
+++ src/main/java/com/xc/api/service/impl/FileFrontServiceImpl.java
@@ -1,15 +1,22 @@
 package com.xc.api.service.impl;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.xc.api.service.FileFrontService;
 import com.xc.constant.Constant;
-import com.xc.entity.FileInfo;
+import com.xc.entity.Directory;
 import com.xc.entity.Note;
 import com.xc.logic.DirectoryLogic;
 import com.xc.logic.NoteLogic;
 import com.xc.util.JsonUtil;
 import com.xc.util.page.Pagination;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by Administrator on 2017/02/27 0027.
@@ -25,11 +32,31 @@ public class FileFrontServiceImpl implements FileFrontService {
 	@Override
 	@GetMapping(value = "/query", consumes = "*/*")
 	public String getFileByDirId(@RequestParam(value = "id", required = false) String id) {
-		FileInfo fileInfo = new FileInfo();
-		fileInfo.setDirs(dirLogic.getDirsByParentId(id));
-		Pagination<Note> notes = noteLogic.getNotesList(null, id, null, 1, Integer.MAX_VALUE, null, null);
-		fileInfo.setNotes(notes.getData());
-		fileInfo.setTotalSize(fileInfo.getDirs().size() + fileInfo.getNotes().size());
-		return JsonUtil.includePropToJson(fileInfo);
+		if ("#".equals(id)) {
+			id = null;
+		}
+		List<Directory> dirs = dirLogic.getDirsByParentId(id);
+		Pagination<Note> pagination = noteLogic.getNotesList(null, id, null, 1, Integer.MAX_VALUE, null, null);
+		List<Note> notes = pagination.getData();
+
+		JSONArray ret = new JSONArray();
+		if (dirs != null && dirs.size() > 0) {
+			for (Directory dir : dirs) {
+				JSONObject object = new JSONObject();
+				object.put("id", dir.getId());
+				object.put("text", dir.getName());
+				object.put("children", true);
+				ret.add(object);
+			}
+		}
+		if (notes != null && notes.size() > 0) {
+			for (Note note : notes) {
+				JSONObject object = new JSONObject();
+				object.put("id", note.getId());
+				object.put("text", note.getTitle());
+				ret.add(object);
+			}
+		}
+		return JsonUtil.includePropToJson(ret);
 	}
 }
